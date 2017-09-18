f41a95e920087dd62971c1047f91653e2f7f4798
Stephen Afam-Osemene <stephenafamo@gmail.com>
2017-09-18 10:45:21 +0100

    modify frontpage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,15 +79,12 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel 2FA Starter
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://stephenafamo.com">Creator</a>
+                    <a href="https://github.com/stephenafamo/laravel-2fa-starter">Source Code</a>
                 </div>
             </div>
         </div>
